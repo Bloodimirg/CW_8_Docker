c6217abda9e710a2100fe702211495b9df8cdb47
Bloodimirg <bloodimirg@gmail.com>
2024-10-14 20:28:47 +0300

    докстринг

--- users/models.py
+++ users/models.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(models.Model):
+class User(AbstractUser):
+    """Модель пользователя"""
+
     username = None
     email = models.EmailField(
         unique=True, verbose_name="Почта", help_text="Укажите почту"
