43c6f7d95e5b6ce52f47c08c1dd5e95cfeccd2b0
Bloodimirg <bloodimirg@gmail.com>
2024-10-14 20:23:30 +0300

    создание пользователя

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -5,8 +5,9 @@ from users.models import User
 
 class Command(BaseCommand):
     """Создание пользователей"""
+
     def handle(self, *args, **options):
-        user = User.objects.create(email="user@mail.ru")
+        user = User.objects.create(email="admin@mail.ru")
         user.set_password("123qwe")
         user.is_active = True
         user.is_staff = True
