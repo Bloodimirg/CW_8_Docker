9e45411e8f8dcebb4a817e60d19c8f516d26385e
Bloodimirg <bloodimirg@gmail.com>
2024-10-14 20:34:26 +0300

    dockstrings

--- users/views.py
+++ users/views.py
@@ -1,22 +1,20 @@
-from rest_framework.generics import (
-    CreateAPIView,
-)
+from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
-
-from users.models import User
 from users.serializers import UserSerializer, CustomTokenObtainPairSerializer
 
 
 class UserCreateApiView(CreateAPIView):
+    """Создание пользователя"""
+
     serializer_class = UserSerializer
-    queryset = User.objects.all()
     permission_classes = (AllowAny,)
 
     def perform_create(self, serializer):
-        user = serializer.save(is_active=True)
-        user.set_password(user.password)
-        user.save()
+        serializer.save()
+
 
 class CustomTokenObtainPairView(TokenObtainPairView):
+    """Получение пары токенов access/refresh"""
+
     serializer_class = CustomTokenObtainPairSerializer
