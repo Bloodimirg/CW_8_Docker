f572ed6da6146db66b635f288f28f8acf1fd000f
Bloodimirg <bloodimirg@gmail.com>
2024-10-14 20:30:51 +0300

    сериализаторы для пользователей и токена

--- users/serializers.py
+++ users/serializers.py
@@ -1,20 +1,41 @@
-from rest_framework import serializers
 from users.models import User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from django.utils import timezone
 
+from rest_framework import serializers
+
+
 class UserSerializer(serializers.ModelSerializer):
     """Сериализатор пользователя"""
+
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ("id", "email", "phone", "country", "avatar", "password")
+        fields = ["email", "password"]
+
+    def create(self, validated_data):
+        user = User(email=validated_data["email"])
+        user.set_password(validated_data["password"])  # Хешируем пароль
+        user.is_active = True  # Устанавливаем пользователя активным
+        user.save()
+        return user
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError(
+                "Пользователь с таким email уже существует."
+            )
+        return value
+
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    """Сериализатор получения токена access и refresh"""
+
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
 
-        # Обновляем поле last_login
         user.last_login = timezone.now()
         user.save()
 
