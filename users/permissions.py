741944ba1a61ca9c3ce87c0ee436fb57a86a7002
Bloodimirg <bloodimirg@gmail.com>
2024-10-14 20:29:12 +0300

    докстринги

--- users/permissions.py
+++ users/permissions.py
@@ -3,11 +3,13 @@ from rest_framework import permissions
 
 class IsModerator(permissions.BasePermission):
     """Проверка на модератора"""
+
     def has_permission(self, request, view):
         return request.user.groups.filter(name="moderator").exists()
 
 
 class IsOwner(permissions.BasePermission):
-    """Проверка на автора"""
+    """Проверка на создателя"""
+
     def has_object_permission(self, request, view, obj):
         return obj.owner == request.user
