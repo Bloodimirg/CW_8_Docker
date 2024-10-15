8cc408bc80db837590001dfd32b7f107f120cbac
Bloodimirg <bloodimirg@gmail.com>
2024-10-15 11:26:59 +0300

    Написаны тесты users

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+
+class UserCreateApiViewTestCase(APITestCase):
+    """Тестирование представления создания пользователя"""
+
+    def setUp(self):
+        # Создание тестового пользователя
+        self.existing_user = User.objects.create(
+            email="existinguser@example.com",
+            password="testpassword"
+        )
+
+        # URL для тестирования
+        self.url = reverse('users:register')
+
+        # Валидные данные для создания пользователя
+        self.valid_payload = {
+            'email': 'newuser@example.com',
+            'password': 'strong_password_123',
+        }
+
+    def test_create_user_valid(self):
+        response = self.client.post(self.url, self.valid_payload)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(User.objects.filter(email=self.valid_payload['email']).exists())
+
+    def test_str_method(self):
+        # Проверяем, что метод __str__() возвращает правильное значение
+        self.assertEqual(str(self.existing_user), "existinguser@example.com")
