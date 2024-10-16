ec70adb15c5b9410023956c132ccc6188112e310
Bloodimirg <bloodimirg@gmail.com>
2024-10-16 18:35:29 +0300

    изменено поведение perform_create

--- users/views.py
+++ users/views.py
@@ -1,17 +1,21 @@
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+from users.models import User
 from users.serializers import UserSerializer, CustomTokenObtainPairSerializer
 
 
 class UserCreateApiView(CreateAPIView):
     """Создание пользователя"""
-
     serializer_class = UserSerializer
     permission_classes = (AllowAny,)
 
     def perform_create(self, serializer):
-        serializer.save()
+        chat_id = self.request.data.get('chat_id')
+        user = serializer.save(is_active=True, chat_id=chat_id)
+        user.set_password(user.password)
+        user.save()
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
