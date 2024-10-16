ce3c80e96570e0d2801f77fda98f9e6d8cc44636
Bloodimirg <bloodimirg@gmail.com>
2024-10-16 18:33:15 +0300

    добавлен метод get_permissions

--- habit/views.py
+++ habit/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, AllowAny
+
+from users.permissions import IsModerator, IsOwner
 from .models import Habit
 from .paginations import CustomPagination
 from .serializers import HabitSerializer, PublicHabitSerializer
@@ -14,6 +16,15 @@ class UserHabitViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         # Возвращает только привычки текущего пользователя
         return Habit.objects.filter(owner=self.request.user)
+
+    def get_permissions(self):
+        if self.action == 'create':
+            self.permission_classes = (~IsModerator,)  # создавать могут не модераторы
+        elif self.action in ['update', 'retrieve', 'partial_update']:
+            self.permission_classes = (IsModerator | IsOwner,)  # обновлять может модератор или владелец
+        elif self.action == 'destroy':
+            self.permission_classes = (~IsModerator | IsOwner,)  # удалять может не модератор или владелец
+        return super().get_permissions()
 
 
 class PublicHabitViewSet(viewsets.ReadOnlyModelViewSet):
